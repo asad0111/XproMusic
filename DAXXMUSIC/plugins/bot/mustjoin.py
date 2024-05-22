1385f52731ed436eca3bf1f7ff07f74d5c8bbb3b
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-05-23 02:30:14 +0530

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "ALLTYPECC"
+MUST_JOIN = "𝗕𝗥𝗢𝗞𝗘𝗡 𝗫 𝗡𝗘𝗧𝗪𝗢𝗥𝗞"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -17,7 +17,7 @@ async def must_join_channel(app: Client, msg: Message):
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/mrbrokenxworld" + MUST_JOIN
+                link = "https://t.me/+b3FFOs5s3uYyMTll" + MUST_JOIN
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link

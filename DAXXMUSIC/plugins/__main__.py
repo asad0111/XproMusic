644bdf1ab49c25677f0e281f3989685f38292644
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-05-24 20:42:13 +0530

    Update __main__.py

--- DAXXMUSIC/plugins/__main__.py
+++ DAXXMUSIC/plugins/__main__.py
@@ -1,8 +1,8 @@
 import asyncio
 import importlib
 from pyrogram import idle
-from EQUROBOT import app
-from EQUROBOT.modules import ALL_MODULES
+from DAXXMUSIC import app
+from DAXXMUSIC.modules import ALL_MODULES
 import config
 from config import LOGGER_ID
 
@@ -12,13 +12,12 @@ loop = asyncio.get_event_loop()
 
 async def daxxpapa_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("EQUROBOT.modules." + all_module)
+        importlib.import_module("DAXXMUSIC.modules." + all_module)
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
-    await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [𝐌𝚁°᭄𝐃𝙰𝚇𝚇 ࿐™ ](https://t.me/iam_daxx)**")
+    await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [𝗠𝗥 𝘅 𝗕𝗥𝗢𝗞𝗘𝗡 ™ ](https://t.me/MRBROKN)**")
     await idle()
-    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @iam_daxx")
+    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @MRBROKN")
   
   
 if __name__ == "__main__":
-    loop.run_until_complete(daxxpapa_boot())
-    
+    loop.run_until_complete(MRBROKNBOT())

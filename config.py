abde46d5843fea4c050e862d45d9728fc9868d88
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-05-31 19:15:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ ASSUSERNAME = getenv("ASSUSERNAME" , "Assis_yumi")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://asadinloveas:asadinloveas@cluster0.afg8tut.mongodb.net/?retryWrites=true&w=majority
-")
+MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://asadinloveas:asadinloveas@cluster0.afg8tut.mongodb.net/?retryWrites=true&w=majority
+)
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
